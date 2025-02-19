d047faf26612f45923aaecd0bc04be05bbf367f8
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-02-19 19:55:37 +0100

    add libsbml compilation to build

--- build.rs
+++ build.rs
@@ -1,23 +1,26 @@
 fn main() -> miette::Result<()> {
-    // Set the link search path to where libSBML is installed.
-    println!("cargo:rustc-link-search=/opt/homebrew/Cellar/libsbml/5.20.4/lib");
+    // Rebuild if these files change.
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Build the libsbml submodule using cmake
+    let dst = cmake::Config::new("libsbml")
+        .always_configure(false) // Only reconfigure when files change.
+        .build();
+
+    // Add the built library to linker search path
+    println!("cargo:rustc-link-search={}/lib", dst.display());
 
     // Link against the actual libSBML library.
     println!("cargo:rustc-link-lib=sbml");
 
-    // Rebuild if these files change.
-    println!("cargo:rerun-if-changed=build.rs");
-
     // Build the autocxx generated wrapper.
-    let mut b = autocxx_build::Builder::new(
-        "src/lib.rs",
-        &[
-            ".",
-            "/opt/homebrew/Cellar/libsbml/5.20.4/include/",
-            "src/utils.hpp",
-        ],
-    )
-    .build()?;
+    let rs_file = "src/lib.rs";
+    let sbml_include = format!("{}/include", dst.display());
+    let utils_hpp = format!("{}/utils.hpp", sbml_include);
+    let lib_root = ".";
+
+    let mut b =
+        autocxx_build::Builder::new(rs_file, &[lib_root, &sbml_include, &utils_hpp]).build()?;
     b.flag_if_supported("-std=c++17").compile("libsbml");
 
     Ok(())
