49cb306ae7d4c5e163b0208caa1d227b543fe370
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-02-19 20:18:24 +0100

    use prelude in doctest

--- src/annotation.rs
+++ src/annotation.rs
@@ -10,7 +10,7 @@
 //! # Example
 //! ```no_run
 //! use serde::{Serialize, Deserialize};
-//! use sbml::{SBMLDocument};
+//! use sbml::prelude::*;
 //!
 //! #[derive(Serialize, Deserialize)]
 //! struct MyAnnotation {
