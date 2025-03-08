b35321d3b48219f93c2b36092cf6a1102a44171f
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-03-08 13:58:18 +0100

    update example

--- examples/create.rs
+++ examples/create.rs
@@ -1,5 +1,4 @@
-use sbml::prelude::*;
-use serde::{Deserialize, Serialize};
+use sbml::{prelude::*, unit::UnitKind};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let doc = SBMLDocument::new(3, 2);
@@ -7,24 +6,50 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create a model
     let model = doc.create_model("example");
 
+    // Create a unit definition
+    let mole = model
+        .build_unit_definition("mole", "mole")
+        .unit(UnitKind::Mole, Some(1), None, None, None)
+        .build();
+
+    let ml = model
+        .build_unit_definition("ml", "ml")
+        .unit(UnitKind::Litre, Some(1), Some(-3), None, None)
+        .build();
+
     // Create a compartment
-    let compartment = model.build_compartment("cytosol").name("cytosol").build();
+    let compartment = model
+        .build_compartment("cytosol")
+        .name("cytosol")
+        .unit(&ml.id())
+        .build();
 
-    // Create an annotation for the glucose species
-    let glucose_annotation = MyAnnotation {
-        xmlns: "http://my.namespace.com".to_string(),
-        key: "test".to_string(),
-        value: 1,
-    };
-
-    // Create the glucose species with the annotation
-    let glucose = model
-        .build_species("glucose")
-        .name("Glucose")
+    // Create the ethanol species
+    let ethanol = model
+        .build_species("ethanol")
+        .name("Ethanol")
         .compartment(&compartment.id())
-        .initial_amount(10.0)
-        .boundary_condition(true)
-        .annotation_serde(&glucose_annotation)?
+        .initial_concentration(0.5)
+        .unit(&mole.id())
+        .has_only_substance_units(false)
+        .build();
+
+    // Create the aldehyde species
+    let aldehyde = model
+        .build_species("aldehyde")
+        .name("Aldehyde")
+        .compartment(&compartment.id())
+        .initial_concentration(0.5)
+        .unit(&mole.id())
+        .has_only_substance_units(false)
+        .build();
+
+    // Create the reaction
+    model
+        .build_reaction("reaction")
+        .name("Reaction")
+        .reactant(&ethanol.id(), 1.0)
+        .product(&aldehyde.id(), 1.0)
         .build();
 
     // Serialize the document to an SBML string
@@ -33,17 +58,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Print the SBML string
     println!("{}", sbml_string);
 
-    // This is how you could also extract annotations from the model
-    let glucose_annot: MyAnnotation = glucose.get_annotation_serde()?;
-    println!("Glucose annotation: {:#?}", glucose_annot);
-
     Ok(())
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct MyAnnotation {
-    #[serde(rename = "@xmlns")]
-    xmlns: String,
-    key: String,
-    value: i32,
 }
