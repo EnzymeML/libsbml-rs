7de313c0fe41e61a7035b54c37ed1fae0d92d61d
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-05-03 15:37:06 +0200

    add read and write test

---
+++ tests/e2e.rs
@@ -0,0 +1,119 @@
+#[cfg(test)]
+mod tests {
+    use sbml::prelude::*;
+
+    #[test]
+    fn test_sbmldoc_write_and_read() {
+        // Arrange
+        let doc = create_doc();
+
+        // Act
+        let xml_string = doc.to_xml_string();
+        let doc2 = SBMLReader::from_xml_string(&xml_string);
+        let xml_string2 = doc2.to_xml_string();
+
+        // Assert
+        assert_eq!(xml_string, xml_string2);
+    }
+
+    fn create_doc() -> SBMLDocument<'static> {
+        let doc = SBMLDocument::default();
+        let model = doc.create_model("test_model");
+
+        // Add a unit definition
+        let ml = model
+            .build_unit_definition("ml", "milliliter")
+            .unit(UnitKind::Litre, Some(1), Some(-3), None, None)
+            .build();
+
+        let mole_l = model
+            .build_unit_definition("mole", "mole")
+            .unit(UnitKind::Mole, Some(1), Some(0), Some(1.0), None)
+            .unit(UnitKind::Litre, Some(-1), Some(0), Some(1.0), None)
+            .build();
+
+        let kelvin = model
+            .build_unit_definition("kelvin", "kelvin")
+            .unit(UnitKind::Kelvin, Some(1), Some(0), Some(1.0), None)
+            .build();
+
+        // Add a compartment
+        let compartment = model
+            .build_compartment("compartment")
+            .name("compartment")
+            .constant(true)
+            .unit(&ml)
+            .volume(1.0)
+            .build();
+
+        // Add a species
+        let substrate = model
+            .build_species("species")
+            .name("species")
+            .constant(false)
+            .has_only_substance_units(false)
+            .compartment(&compartment)
+            .initial_concentration(1.0)
+            .unit(&mole_l)
+            .build();
+
+        let product = model
+            .build_species("product")
+            .name("product")
+            .constant(false)
+            .has_only_substance_units(false)
+            .compartment(&compartment)
+            .initial_concentration(1.0)
+            .unit(&mole_l)
+            .build();
+
+        // Add a reaction
+        let reaction = model
+            .build_reaction("reaction")
+            .name("reaction")
+            .reactant(&substrate, 1.0)
+            .product(&product, 1.0)
+            .build();
+
+        // Add a kinetic law
+        let kinetic_law = reaction.create_kinetic_law("substrate * kcat / (substrate + Km)");
+
+        // Add local parameters
+        kinetic_law
+            .build_local_parameter("kcat")
+            .units(&mole_l.id())
+            .value(1.0)
+            .build();
+
+        kinetic_law
+            .build_local_parameter("Km")
+            .units(&mole_l.id())
+            .value(1.0)
+            .build();
+
+        // Add a parameter (global)
+        model
+            .build_parameter("T")
+            .units(&kelvin)
+            .value(310.0)
+            .build();
+
+        model
+            .build_parameter("Km")
+            .units(&mole_l)
+            .value(1.0)
+            .build();
+
+        // Add a rate rule
+        model
+            .build_rate_rule(&product, "kcat * substrate / (substrate + Km)")
+            .build();
+
+        // Add an assignment rule
+        model
+            .build_assignment_rule("x", "T * kcat * substrate / (T + Km)")
+            .build();
+
+        doc
+    }
+}
