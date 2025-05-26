14cd24497884bd3fed8ecff4ab603e15007923bd
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-05-26 16:31:31 +0200

    update test case

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -6,7 +6,7 @@ mod tests {
     fn test_sbmldoc_debug() {
         let doc = create_doc();
         let debug_string = format!("{:?}", doc);
-        insta::assert_snapshot!(debug_string, @r#"SBMLDocument { level: 3, version: 2, model: Some(Model { id: "test_model", name: "", list_of_species: [Species { id: "species", name: Some("species"), compartment: Some("compartment"), initial_amount: None, initial_concentration: Some(1.0), unit: Some("mole"), boundary_condition: Some(false), constant: false, has_only_substance_units: Some(false) }, Species { id: "product", name: Some("product"), compartment: Some("compartment"), initial_amount: None, initial_concentration: Some(1.0), unit: Some("mole"), boundary_condition: Some(false), constant: false, has_only_substance_units: Some(false) }], list_of_compartments: [Compartment { id: "compartment", name: Some("compartment"), spatial_dimensions: None, unit: Some("ml"), size: Some(1.0), volume: Some(1.0), outside: None, constant: Some(true) }], list_of_unit_definitions: [UnitDefinition { id: "ml", name: Some("milliliter"), units: [Unit { kind: Litre, exponent: 1, multiplier: 1.0, scale: -3, offset: 0.0 }] }, UnitDefinition { id: "mole", name: Some("mole"), units: [Unit { kind: Mole, exponent: 1, multiplier: 1.0, scale: 0, offset: 0.0 }, Unit { kind: Litre, exponent: -1, multiplier: 1.0, scale: 0, offset: 0.0 }] }, UnitDefinition { id: "kelvin", name: Some("kelvin"), units: [Unit { kind: Kelvin, exponent: 1, multiplier: 1.0, scale: 0, offset: 0.0 }] }], list_of_reactions: [Reaction { id: "reaction", name: Some("reaction"), reversible: None, compartment: None, reactants: RefCell { value: [SpeciesReference { species: "species", stoichiometry: 1.0, constant: false }] }, products: RefCell { value: [SpeciesReference { species: "product", stoichiometry: 1.0, constant: false }] }, modifiers: RefCell { value: [] } }], list_of_parameters: [Parameter { id: "T", name: None, value: Some(310.0), units: Some("kelvin"), constant: Some(true) }, Parameter { id: "Km", name: None, value: Some(1.0), units: Some("mole"), constant: Some(true) }], list_of_rate_rules: [Rule { type: Ok(RateRule), variable: "product", formula: "kcat * substrate / (substrate + Km)" }], list_of_assignment_rules: [Rule { type: Ok(AssignmentRule), variable: "x", formula: "T * kcat * substrate / (T + Km)" }] }) }"#);
+        insta::assert_snapshot!(debug_string, @r#"SBMLDocument { level: 3, version: 2, model: Some(Model { id: "test_model", name: "", list_of_species: [Species { id: "species", name: Some("species"), compartment: Some("compartment"), initial_amount: None, initial_concentration: Some(1.0), unit: Some("mole"), boundary_condition: Some(false), constant: false, has_only_substance_units: Some(false) }, Species { id: "product", name: Some("product"), compartment: Some("compartment"), initial_amount: None, initial_concentration: Some(1.0), unit: Some("mole"), boundary_condition: Some(false), constant: false, has_only_substance_units: Some(false) }], list_of_compartments: [Compartment { id: "compartment", name: Some("compartment"), spatial_dimensions: None, unit: Some("ml"), size: Some(1.0), volume: Some(1.0), outside: None, constant: Some(true) }], list_of_unit_definitions: [UnitDefinition { id: "ml", name: Some("milliliter"), units: [Unit { kind: Litre, exponent: 1, multiplier: 1.0, scale: -3, offset: 0.0 }] }, UnitDefinition { id: "mole", name: Some("mole"), units: [Unit { kind: Mole, exponent: 1, multiplier: 1.0, scale: 0, offset: 0.0 }, Unit { kind: Litre, exponent: -1, multiplier: 1.0, scale: 0, offset: 0.0 }] }, UnitDefinition { id: "kelvin", name: Some("kelvin"), units: [Unit { kind: Kelvin, exponent: 1, multiplier: 1.0, scale: 0, offset: 0.0 }] }], list_of_reactions: [Reaction { id: "reaction", name: Some("reaction"), reversible: None, compartment: None, reactants: RefCell { value: [SpeciesReference { species: "species", stoichiometry: 1.0, constant: false }] }, products: RefCell { value: [SpeciesReference { species: "product", stoichiometry: 1.0, constant: false }] }, modifiers: RefCell { value: [] } }], list_of_parameters: [Parameter { id: "T", name: None, value: Some(310.0), units: Some("kelvin"), constant: Some(true) }, Parameter { id: "Km", name: None, value: Some(1.0), units: Some("mole"), constant: Some(true) }], list_of_rate_rules: [Rule { type: Ok(RateRule), variable: "product", formula: "kcat * substrate / (substrate + Km)" }], list_of_assignment_rules: [Rule { type: Ok(AssignmentRule), variable: "x", formula: "T * kcat * substrate / (T + Km)" }], list_of_objectives: [], list_of_flux_bounds: [FluxBound { id: Some("fb1"), reaction: Some("reaction"), operation: LessEqual }] }) }"#);
     }
 
     #[test]
@@ -120,6 +120,19 @@ mod tests {
         model
             .build_assignment_rule("x", "T * kcat * substrate / (T + Km)")
             .build();
+
+        // Add an objective
+        let objective = Objective::new(&model, "objective", ObjectiveType::Maximize)
+            .expect("Failed to create objective");
+
+        objective
+            .create_flux_objective("fo1", "reaction", 1.0)
+            .expect("Failed to create flux objective");
+
+        // Create the flux bound
+        model
+            .create_flux_bound("fb1", "reaction", FluxBoundOperation::LessEqual)
+            .expect("Failed to create flux bound");
 
         doc
     }
