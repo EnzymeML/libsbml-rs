23121c59f94ddcca7ccc3a505424f3b73f6e0d86
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-05-29 12:23:03 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -310,8 +310,9 @@ fn build_libcombine(
     }
 
     // Point to the zipper library we just built
-    let zipper_lib = find_zipper_lib_file(&out_dir.join("lib").to_str().unwrap())
-        .expect("Failed to find zipper library");
+    let lib_dir = std::path::Path::new(&out_dir).join("lib");
+    let zipper_lib =
+        find_zipper_lib_file(lib_dir.to_str().unwrap()).expect("Failed to find zipper library");
     println!("cargo:warning=zipper_lib: {:?}", zipper_lib);
     config.define("ZIPPER_LIBRARY", zipper_lib.to_str().unwrap());
     config.define("ZIPPER_INCLUDE_DIR", format!("{}/include", out_dir));
